3d96ef5c9d5d74438080b0c329be00b67b8a0a59
galargh <piotr.galar@gmail.com>
2023-08-02 19:13:49 +0100

    feat: implement getChannelsWithLastMessageTimestamp

--- src/Code.ts
+++ src/Code.ts
@@ -1,224 +1,90 @@
 const _ = LodashGS.load();
 
-const SCRIPT_ID = ScriptApp.getScriptId();
-const CLIENT_ID = PropertiesService.getScriptProperties().getProperty(
-  "CLIENT_ID"
-) as string;
-const CLIENT_SECRET = PropertiesService.getScriptProperties().getProperty(
-  "CLIENT_SECRET"
-) as string;
-
-const CLASSIC_CLIENT_ID = PropertiesService.getScriptProperties().getProperty(
-  "CLASSIC_CLIENT_ID"
-) as string;
-const CLASSIC_CLIENT_SECRET = PropertiesService.getScriptProperties().getProperty(
-  "CLASSIC_CLIENT_SECRET"
-) as string;
-
-// const SCOPE = 'channels:history,channels:read,groups:history,groups:read,usergroups:read,users:read,users:read.email'
-const USER_SCOPE =
-  "channels:history,channels:read,groups:history,groups:read,search:read,usergroups:read,users:read,users:read.email";
-const REDIRECT_URI = `https://script.google.com/macros/d/${SCRIPT_ID}/usercallback`;
-
-function onOpen(e) {
+function onOpen() {
   SpreadsheetApp.getUi()
     .createMenu("Slack")
-    .addItem("Authorize IPDX", "authorize")
-    .addItem("Reset IPDX Authorization", "reset")
-    .addItem("Authorize IPDX (Classic)", "authorizeClassic")
-    .addItem("Reset IPDX (Classic) Authorization", "resetClassic")
+    .addItem("Authorize", "_onAuthorize")
+    .addItem("Reset Authorization", "_onReset")
+    .addItem("Check Authorization", "_onCheck")
     .addToUi();
 }
 
-function getOAuthService() {
-  return OAuth2.createService("slack")
-    .setAuthorizationBaseUrl("https://slack.com/oauth/v2/authorize")
-    .setTokenUrl("https://slack.com/api/oauth.v2.access")
-    .setCallbackFunction("authCallback")
-    .setPropertyStore(PropertiesService.getScriptProperties())
-    .setParam("user_scope", USER_SCOPE)
-    .setClientId(CLIENT_ID)
-    .setClientSecret(CLIENT_SECRET);
+function _onAuthorize() {
+  const authorizationUrl = Slack.getOrCreate("ipdx").getAuthorizationUrl();
+  SpreadsheetApp.getUi().showModalDialog(
+    HtmlService.createHtmlOutput(`<script>function authorize() { window.open("${authorizationUrl}", '_blank').focus(); google.script.host.close(); }</script><button onclick="authorize()">Authorize</button>`),
+    "Authorize"
+  )
 }
 
-function getClassicOAuthService() {
-  return OAuth2.createService("slack-classic")
-    .setAuthorizationBaseUrl("https://slack.com/oauth/authorize")
-    .setTokenUrl("https://slack.com/api/oauth.access")
-    .setCallbackFunction("authCallbackClassic")
-    .setPropertyStore(PropertiesService.getScriptProperties())
-    .setScope(USER_SCOPE)
-    .setClientId(CLASSIC_CLIENT_ID)
-    .setClientSecret(CLASSIC_CLIENT_SECRET);
+function _onReset() {
+  Slack.getOrCreate("ipdx").reset();
 }
 
-function authCallback(callbackRequest) {
-  const service = getOAuthService();
-  const authorized = service.handleCallback(callbackRequest);
-  let template;
-  if (authorized) {
-    const accessToken = JSON.parse(PropertiesService.getScriptProperties().getProperty("oauth2.slack")).authed_user.access_token;
-    PropertiesService.getScriptProperties().setProperty("SLACK_USER_ACCESS_TOKEN", accessToken);
-    template = HtmlService.createTemplateFromFile("Success");
-  } else {
-    template = HtmlService.createTemplateFromFile("Failure");
-    template.code = JSON.stringify(service.getAccessToken(), null, 2);
-  }
-  const page = template.evaluate();
-  SpreadsheetApp.getUi().showModalDialog(page, "Authorization Result");
-  return page;
+function _onCheck() {
+  const hasAccess = Slack.getOrCreate("ipdx").hasAccess();
+  SpreadsheetApp.getUi().alert(hasAccess ? "Authorized" : "Not Authorized");
 }
 
-function authCallbackClassic(callbackRequest) {
-  const service = getClassicOAuthService();
-  const authorized = service.handleCallback(callbackRequest);
-  let template;
-  if (authorized) {
-    const accessToken = service.getAccessToken();
-    PropertiesService.getScriptProperties().setProperty("SLACK_USER_ACCESS_TOKEN", accessToken);
-    template = HtmlService.createTemplateFromFile("Success");
-  } else {
-    template = HtmlService.createTemplateFromFile("Failure");
-    template.code = JSON.stringify(service.getAccessToken(), null, 2);
-  }
-  const page = template.evaluate();
-  SpreadsheetApp.getUi().showModalDialog(page, "Authorization Result");
-  return page;
+function getChannels() {
+  const IPDX = Slack.getOrCreate("ipdx")
+  const publicChannels = IPDX.getPaginated("conversations.list", "types", "public_channel", "exclude_archived", "true");
+  const privateChannels = IPDX.getPaginated("conversations.list", "types", "public_channel", "exclude_archived", "true");
+  let channels = [...publicChannels, ...privateChannels];
+  channels = flatMap(channels, "channels");
+  channels = mapReduce(channels, "id", "name", "is_private", "num_members");
+  return toArray(channels);
 }
 
-function getToken() {
-  const service = getOAuthService();
-  return service.getToken();
-}
+function getChannelsWithLastMessageTimestamp() {
+  const IPDX = Slack.getOrCreate("ipdx")
+  const publicChannels = IPDX.getPaginated("conversations.list", "types", "public_channel", "exclude_archived", "true");
+  const privateChannels = IPDX.getPaginated("conversations.list", "types", "public_channel", "exclude_archived", "true");
+  let channels = [...publicChannels, ...privateChannels];  channels = flatMap(channels, "channels");
+  channels = mapReduce(channels, "id", "name", "is_private", "num_members", "is_member");
 
-function getAccessToken() {
-  const service = getOAuthService();
-  return service.getAccessToken();
-}
-
-function getClassicToken() {
-  const service = getClassicOAuthService();
-  return service.getToken();
-}
-
-function getClassicAccessToken() {
-  const service = getClassicOAuthService();
-  return service.getAccessToken();
-}
-
-function authorize() {
-  const service = getOAuthService();
-  const template = HtmlService.createTemplateFromFile("Authorize");
-  template.authorizationUrl = service.getAuthorizationUrl();
-  const page = template.evaluate();
-  SpreadsheetApp.getUi().showModalDialog(page, "Authorize IPDX");
-}
-
-function authorizeClassic() {
-  const service = getClassicOAuthService();
-  const template = HtmlService.createTemplateFromFile("Authorize");
-  template.authorizationUrl = service.getAuthorizationUrl();
-  const page = template.evaluate();
-  SpreadsheetApp.getUi().showModalDialog(page, "Authorize IPDX (Classic)");
-}
-
-function reset() {
-  const service = getOAuthService();
-  service.reset();
-}
-
-function resetClassic() {
-  const service = getClassicOAuthService();
-  service.reset();
-}
-
-function _get(endpoint, ...args) {
-  const store = PropertiesService.getScriptProperties();
-  let url = `https://slack.com/api/${endpoint}`;
-  if (args) {
-    let separator = "?";
-    for (const arg of args) {
-      url += `${separator}${encodeURIComponent(arg)}`;
-      separator = separator === "=" ? "&" : "=";
+  channels = channels.map(channel => {
+    if (channel.is_member) {
+      let messages = IPDX.get("conversations.history", "channel", channel.id);
+      messages = flatMap(messages, "messages");
+      messages = mapReduce(messages, "ts", "type");
+      channel.last_message_ts = messages.find((message: any) => message.type === "message")?.ts;
+      return channel;
+    } else {
+      return channel;
     }
-  }
-  console.log(url);
-  const headers = {
-    Authorization: `Bearer ${store.getProperty("SLACK_USER_ACCESS_TOKEN")}`,
-    "Content-Type": "application/x-www-form-urlencoded",
-  };
-  console.log(headers);
-  const response = UrlFetchApp.fetch(url, { headers });
-  const text = response.getContentText();
-  const json = JSON.parse(text);
-  if (!json.ok) {
-    throw new Error(json.error);
-  }
-  return json;
+  })
+
+  return toArray(channels);
 }
 
-function get(endpoint, key, ...args) {
-  let response = _get(endpoint, ...args);
-  console.log(response);
-  if (!args.includes("limit")) {
-    while (response?.response_metadata?.next_cursor) {
-      const nextResponse = _get(
-        endpoint,
-        "cursor",
-        response.response_metadata.next_cursor,
-        ...args
-      );
-      response = _.mergeWith(response, nextResponse, (objValue, srcValue) => {
-        if (_.isArray(objValue)) {
-          return objValue.concat(srcValue);
-        }
-      });
-    }
+function flatMap(data: any[], ...args: any[]): any[] {
+  if (!_.isArray(data)) {
+    data = [data];
   }
-  if (key) {
-    if (!_.isArray(key)) {
-      key = [key];
-    }
-    for (const part of key) {
-      response = response[part];
-    }
-  }
-  if (!_.isArray(response)) {
-    response = [response];
-  }
-  console.log(response);
-  const headers = response.length ? Object.keys(response[0]) : [];
-  const rows = [headers];
-  response = response.map((item: any) => {
-    const row: any[] = [];
-    for (const header of headers) {
-      const value = item[header];
-      if (_.isObject(value)) {
-        row.push(JSON.stringify(value));
-      } else {
-        row.push(value);
-      }
-    }
-    rows.push(row);
+  return _.flatMap(data, row => {
+    return args.flatMap(arg => row[arg])
   });
-  return rows;
 }
 
-function conversationsList(...args) {
-  return get("conversations.list", "channels", ...args);
+function mapReduce(data: any[], ...args: any[]): any[] {
+  return _.map(data, row => {
+    return _.reduce(args, (acc, arg) => {
+      acc[arg] = row[arg];
+      return acc;
+    }, {});
+  })
 }
 
-function conversationsHistory(...args) {
-  return get("conversations.history", "messages", ...args);
-}
-
-function filterByName(data, ...args) {
-  if (_.isArray(data) && data.length) {
-    const indices = args
-      .map((arg) => data[0].indexOf(arg))
-      .filter((arg) => arg !== -1);
-    return data.map((row) => indices.map((index) => row[index]));
-  } else {
-    return data;
+function toArray(data: any) {
+  const keys = _.uniq(_.flatMap(data, row => Object.keys(row)));
+  const rows = [keys];
+  for (const row of data) {
+    const values = [];
+    for (const key of keys) {
+      values.push(row[key]);
+    }
+    rows.push(values);
   }
+  return rows;
 }
---
+++ src/Slack.ts
@@ -0,0 +1,134 @@
+class Slack {
+  private static instances: { [key: string]: Slack } = {};
+
+  private name: string;
+  private service: GoogleAppsScriptOAuth2.OAuth2Service;
+  private isClassic: boolean;
+
+  private constructor(name: string) {
+    const store = PropertiesService.getScriptProperties();
+
+    const id = store.getProperty(`${name}.id`) as string;
+    const secret = store.getProperty(`${name}.secret`) as string;
+    let scope = store.getProperty(`${name}.scope`) as string;
+    if (scope === '""') {
+      scope = "";
+    }
+    let userScope = store.getProperty(`${name}.userScope`) as string;
+    if (userScope === '""') {
+      userScope = "";
+    }
+
+    const isClassic = userScope === null;
+    const authorizationBaseUrl: string = isClassic ? "https://slack.com/oauth/authorize" : "https://slack.com/oauth/v2/authorize";
+    const tokenUrl: string = isClassic ? "https://slack.com/api/oauth.access" : "https://slack.com/api/oauth.v2.access";
+
+    this.name = name;
+    this.service = OAuth2.createService(name)
+      .setClientId(id)
+      .setClientSecret(secret)
+      .setAuthorizationBaseUrl(authorizationBaseUrl)
+      .setTokenUrl(tokenUrl)
+      .setCallbackFunction("Slack.handleCallback")
+      .setPropertyStore(store)
+      .setScope(scope)
+      .setParam("user_scope", userScope);
+    this.isClassic = isClassic;
+    Slack.instances[name] = this;
+  }
+
+  public static getOrCreate(name: string): Slack {
+    const instance = Slack.instances[name];
+    if (instance === undefined) {
+      return new Slack(name);
+    } else {
+      return instance;
+    }
+  }
+
+  public static handleCallback(callbackRequest: GoogleAppsScript.Events.DoGet): GoogleAppsScript.HTML.HtmlOutput {
+    const instance = Slack.getOrCreate(callbackRequest.parameter.serviceName);
+    if (instance === undefined) {
+      return HtmlService.createHtmlOutput(`Unknown service: ${callbackRequest.parameter.serviceName}`);
+    } else {
+      return instance.handleCallback(callbackRequest);
+    }
+  }
+
+  public handleCallback(callbackRequest: GoogleAppsScript.Events.DoGet): GoogleAppsScript.HTML.HtmlOutput {
+    const authorized = this.service.handleCallback(callbackRequest);
+    if (authorized) {
+      return HtmlService.createHtmlOutput(`Success! ${this.name} is authorized. You can close this tab.`);
+    } else {
+      return HtmlService.createHtmlOutput(`Denied. You can close this tab`);
+    }
+  }
+
+  public getAuthorizationUrl(): string {
+    return this.service.getAuthorizationUrl();
+  }
+
+  public reset(): void {
+    this.service.reset();
+  }
+
+  public hasAccess(): boolean {
+    return this.service.hasAccess();
+  }
+
+  private getAccessToken(): string {
+    return this.service.getAccessToken();
+  }
+
+  private getUserAccessToken(): string {
+    if (this.isClassic) {
+      return this.service.getAccessToken();
+    } else {
+      const store = PropertiesService.getScriptProperties();
+      const value = store.getProperty(`oauth2.${this.name}`);
+      const json = JSON.parse(value);
+      return json.authed_user.access_token;
+    }
+  }
+
+  public get(endpoint: string, ...args: string[]): any {
+    let url = `https://slack.com/api/${endpoint}`;
+    if (args) {
+      let separator = "?";
+      for (const arg of args) {
+        url += `${separator}${encodeURIComponent(arg)}`;
+        separator = separator === "=" ? "&" : "=";
+      }
+    }
+    const headers = {
+      Authorization: `Bearer ${this.getAccessToken()}`,
+      "Content-Type": "application/x-www-form-urlencoded",
+    };
+    console.log({
+      url,
+      headers,
+    })
+    const response = UrlFetchApp.fetch(url, { headers });
+    const text = response.getContentText();
+    const json = JSON.parse(text);
+    if (!json.ok) {
+      throw new Error(json.error);
+    }
+    return json;
+  }
+
+  public getPaginated(endpoint: string, ...args: string[]): any[] {
+    const responses = [this.get(endpoint, ...args)];
+    while (responses[responses.length - 1].response_metadata.next_cursor !== "") {
+      responses.push(
+        this.get(
+          endpoint,
+          "cursor",
+          responses[responses.length - 1].response_metadata.next_cursor,
+          ...args
+        )
+      );
+    }
+    return responses;
+  }
+}
